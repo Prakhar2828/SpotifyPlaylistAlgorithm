c50afaab183283d39452812a9728943faa957cd6
Prakhar2828 <144481424+Prakhar2828@users.noreply.github.com>
2023-12-18 16:46:23 -0500

    Update PlaylistWindow.java

--- SpotifyPlaylist/src/dailymixes/PlaylistWindow.java
+++ SpotifyPlaylist/src/dailymixes/PlaylistWindow.java
@@ -1,10 +1,3 @@
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Prakhar Pandey (prakhar28)
-
 package dailymixes;
 
 import cs2.*;
